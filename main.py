cba3115208e63206a6d26d8907bd8143da229fec
Clamator <kalmah23@gmail.com>
2021-12-14 17:35:24 +0300

    commit before pagination testing

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Headhunter(object):
     def __init__(self):
         option = options = Options()
         option.add_argument("--disable-blink-features=AutomationControlled")
-        # option.headless = True
+        option.headless = True
         self.driver = webdriver.Chrome(chrome_options=option)
         self.driver.get(hh_login_page)
 
@@ -52,10 +52,39 @@ class Headhunter(object):
                 links_list.append(link)
             except:
                 print('no vacancy link')
+        for test_link in links_list:
+            test_button = self.driver.get(test_link)
+            # the button below hits and opens employer contacts so I can parse it
+            employer_contacts_button = self.driver.find_element(By.XPATH,
+                                                                '//span[contains(text(), "Контактная информация")]')
+            self.driver.execute_script("arguments[0].click();", employer_contacts_button)
+            try:
+                vacancy_name = self.driver.find_element(By.CSS_SELECTOR, 'h1[class*="bloko-header-1"]').text.strip()
+            except:
+                vacancy_name = 'unknown vacancy name'
+            try:
+                salary = self.driver.find_element(By.CSS_SELECTOR, 'div[class*="vacancy-salary"]').text.strip()
+            except:
+                salary = 'by agreement'
+            try:
+                company_name = self.driver.find_element(By.CSS_SELECTOR, 'a[class*="vacancy-company-name"]').text.strip()
+            except:
+                company_name = 'unknown company name'
+            try:
+                contact_name = self.driver.find_element(By.CSS_SELECTOR, 'p[data-qa*="vacancy-contacts__fio"]').text.strip()
+            except:
+                contact_name = 'no contacts'
+            try:
+                contact_phone_number = self.driver.find_element(By.CSS_SELECTOR, 'p[data-qa*="vacancy-contacts__phone"]').text.strip()
+            except:
+                contact_phone_number = 'no contacts'
+            try:
+                address = self.driver.find_element(By.CSS_SELECTOR, 'span[data-qa*="vvacancy-view-raw-address"]').text.strip()
+            except:
+                address = 'address is unknown'
 
-        test_button = self.driver.get(links_list[0])
-        vacancy_name = self.driver.find_element(By.CSS_SELECTOR, 'h1[class*="bloko-header-1"]')
-        print(vacancy_name.text)
+            print(vacancy_name, salary, company_name, contact_name, contact_phone_number, address)
+
 
 if __name__ == '__main__':
     hh_login_page = 'https://hh.ru/account/login'
