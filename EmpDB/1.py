6544423e2f28118db5ad2d4b67839db1d31a655f
Clamator <kalmah23@gmail.com>
2022-03-12 20:44:50 +0300

    test memory usage-1

--- EmpDB/1.py
+++ EmpDB/1.py
@@ -5,13 +5,15 @@ import csv
 df2 = pd.Cov = pd.read_csv(r"printer.csv",names=['tui', 'price', 'dot', 'postcode', 'type', 'age', 'duration', 'paon', 'saon', 'street', 'loc', 'city',
               'dist', 'main_dist', 'ppd',  'rec_stat'], parse_dates=['dot'])
 
-df2 = df2[df2.duplicated(['paon', 'saon', 'street', 'loc', 'city',
+df2_gl = df2.select_dtypes(include=['object']).copy()
+
+df2_gl = df2_gl[df2_gl.duplicated(['paon', 'saon', 'street', 'loc', 'city',
               'dist', 'main_dist'], keep='first')]
 
-df2 = df2.drop_duplicates(subset=['paon', 'saon', 'street', 'loc', 'city',
-              'dist', 'main_dist'], keep='first', inplace=False).to_csv('./tt.csv')
+df2_gl = df2_gl.drop_duplicates(subset=['paon', 'saon', 'street', 'loc', 'city',
+              'dist', 'main_dist'], keep='first', inplace=False).to_csv('./t2.csv')
 
-#print(df2.info(memory_usage='deep'))
+#print(df2_gl.info(memory_usage='deep'))
 
 #df2_obj = df2.select_dtypes(include=['object']).copy()
 #df2_obj.describe()
