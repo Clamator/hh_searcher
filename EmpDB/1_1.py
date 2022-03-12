e7da0f008401df7d44d2cd698555df70de827a03
Clamator <kalmah23@gmail.com>
2022-03-12 20:59:11 +0300

    test memory usage-2

--- EmpDB/1_1.py
+++ EmpDB/1_1.py
@@ -2,11 +2,28 @@ import pandas as pd
 import dask.dataframe as dd
 filename = 'printer2.csv'
 
-df = dd.read_table(filename)
+df = dd.read_csv(filename)
+
+df2 = df[['h', 'k']].drop_duplicates().compute() # unique
+
+
 #df.to_parquet('printer2_parc')
-df2 = dd.read_parquet('printer2_parc/part.*.parquet', columns=['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p'])
-print(df2['b'].mean().compute())
+#df2 = dd.read_parquet('printer2_parc/part.*.parquet', columns=['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p'])
+#print(df2['b'].mean().compute())
 
-
+#print(df.mean(2).compute())
+#print(df)
 #print(df['b'].mean().compute())
+#['h', 'i','j','k', 'l', 'm', 'n']
 
+
+import dask.dataframe as dd
+import dask
+
+
+df = pd.DataFrame({"temp1":[1,2,2,4],"temp2":[1,2,3,4]})
+ddf = dd.from_pandas(df,npartitions=2)
+for unique_value in [ddf.temp1.unique(), df.temp2.unique()]:
+    print(unique_value)
+
+print(df)
--- EmpDB/1_test.py
+++ EmpDB/1_test.py
@@ -3,14 +3,14 @@ from sys import getsizeof
 import pandas as pd
 
 gl = pd.read_csv('printer2.csv')
-print(gl.info(memory_usage='deep'))
 gl_obj = gl.select_dtypes(include=['object']).copy()
 
-dow = gl_obj['h']
-print(dow.head())
+dow = gl_obj[['h', 'i','j','k', 'l', 'm', 'n']]
+print(dow.info(memory_usage='deep'))
+#print(dow.head())
 
 dow_cat = dow.astype('category')
-print(dow_cat.info(memory_usage='deep'))
 
-dow2 = gl_obj.a
-print(dow2)
+#dow2 = gl_obj.c
+#print(dow_cat.info(memory_usage='deep'))
+#print(dow2)
