673ebb2ca4f5e10be3ed976baeac83249187663f
Clamator <kalmah23@gmail.com>
2022-03-12 22:21:50 +0300

    test memory usage-3

--- EmpDB/1_chunk.py
+++ EmpDB/1_chunk.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 result = None
-df2 = pd.Cov = pd.read_csv(r"printer.csv",names=['tui', 'price', 'dot', 'postcode', 'type', 'age', 'duration', 'paon', 'saon', 'street', 'loc', 'city',
+df2 = pd.Cov = pd.read_csv(r"pp-complete.csv",names=['tui', 'price', 'dot', 'postcode', 'type', 'age', 'duration', 'paon', 'saon', 'street', 'loc', 'city',
               'dist', 'main_dist', 'ppd',  'rec_stat'], parse_dates=['dot'], chunksize=1000000)
 for chunk in df2:
     voters_street = chunk[['paon', 'saon', 'street', 'loc', 'city',
@@ -13,4 +13,10 @@ for chunk in df2:
         result = result.add(voters_street, fill_value=0)
 
 #result.sort_values(ascending=False, inplace=True)
-print(result)
+result_gl = result.select_dtypes(include=['object']).copy()
+
+result_gl = result_gl[result_gl.duplicated(['paon', 'saon', 'street', 'loc', 'city',
+              'dist', 'main_dist'], keep='first')]
+
+result_gl = result_gl.drop_duplicates(subset=['paon', 'saon', 'street', 'loc', 'city',
+              'dist', 'main_dist'], keep='first', inplace=False).to_csv('./t2.csv')
